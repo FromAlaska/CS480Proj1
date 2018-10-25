033014be8df8d08297ee030b5a5cb4f1b7048f96
Arsh Chauhan <achauhan@alaska.edu>
2018-10-25 10:23:02 -0800

    Added software only debounce. Seems to work

--- stepperCS425/stepperCS425.ino
+++ stepperCS425/stepperCS425.ino
@@ -23,7 +23,10 @@ const int buttonPin3 = 3;
 int buttonState = 0;         // variable for reading the pushbutton status
 int buttonState2 = 0;         // variable for reading the pushbutton status
 
-const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution
+const int stepsPerRevolution = 150;  // change this to fit the number of steps per revolution
+const unsigned long  debounceMillis = 100;
+unsigned long  lastDebounce1 = 0;
+unsigned long  lastDebounce2 = 0;
 // for your motor
 
 // initialize the stepper library on pins 8 through 11:
@@ -40,23 +43,36 @@ void setup() {
 }
 
 void loop() {
-
   // read the state of the pushbutton value:
+  int runOnce=0;
+  int oldButton = buttonState;
+  int oldButton2 = buttonState2;
   buttonState = digitalRead(buttonPin2);
   buttonState2 = digitalRead(buttonPin3);
-
-  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (buttonState == HIGH) {
-    // step one revolution  in one direction:
-    Serial.println("clockwise");
-    myStepper.step(stepsPerRevolution);
-    delay(500);
+  Serial.println(buttonState);
+  Serial.println(buttonState2);
+  if (oldButton != buttonState)
+  {
+      lastDebounce1 = millis();
   }
-  if(buttonState2 == HIGH) {
+  if (oldButton2 != buttonState2)
+  {
+      lastDebounce2 = millis();
+  }
+  if (buttonState == HIGH && ((millis()-lastDebounce1)>debounceMillis) && runOnce==0) {
+      // step one revolution  in one direction:
+      Serial.println("clockwise");
+      myStepper.step(stepsPerRevolution);
+      runOnce=1;
+      delay(10);
+    }
+  
+  if(buttonState2 == HIGH && ((millis()-lastDebounce2)>debounceMillis)&& runOnce==0) {
     // step one revolution in the other direction:
     Serial.println("counterclockwise");
     myStepper.step(-stepsPerRevolution);
-    delay(500);
+    runOnce=1;
+    delay(10);
   }
-  delay(1000);
+  //delay(1000);
 }
